138e3c009b60c910c7389bc4e82e3169fc8dbf0d
Emilien <emilien.derobert@gmail.com>
2026-04-16 20:35:28 +0200

    feat: Calcul discount automatique

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,4 +1,16 @@
+import { useState } from "react"
+
 export default function Article({ onDelete }: { onDelete: () => void }) {
+    const [discounted, setDiscounted] = useState(0);
+
+    function getDiscountedPrice() {
+        const priceInput = document.querySelector("#prix input") as HTMLInputElement;
+        const discountInput = document.querySelector("#remise input") as HTMLInputElement;
+        const price = parseFloat(priceInput.value);
+        const discount = parseFloat(discountInput.value);
+
+        setDiscounted(price - (isNaN(price) ? 0 : price * (isNaN(discount) ? 0 : discount) / 100));
+    }
 
     return (
         <div id="article" className="flex flex-row gap-4">
@@ -12,17 +24,22 @@ export default function Article({ onDelete }: { onDelete: () => void }) {
 
             <div id="prix" className="flex flex-col align-middle gap-1 justify-center text-center">
                 <label>Prix</label>
-                <input type="text" />
+                <input onChange={() => getDiscountedPrice()} type="text" />
             </div>
 
             <div id="remise" className="flex flex-col align-middle gap-1 justify-center text-center">
-                <label>Remise</label>
-                <input type="text" />
+                <label>Remise %</label>
+                <input onChange={() => getDiscountedPrice()} type="text" />
             </div>
 
             <div id="discounted" className="flex flex-col align-middle gap-1 justify-center text-center">
                 <label>Prix Remis</label>
-                <input className="bg-gray-200 cursor-not-allowed" type="text" disabled />
+                <input
+                    value={!isNaN(discounted) ? discounted : 0}
+                    type="text"
+                    className="bg-gray-200 cursor-not-allowed"
+                    disabled
+                />
             </div>
 
             <div id="quantite" className="flex flex-col align-middle gap-1 justify-center text-center">
