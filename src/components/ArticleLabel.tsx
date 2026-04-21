32f3865ddeb61ef823fa5404ea0ea107a962b2be
EmilPhOniX <emilien.derobert@gmail.com>
2026-04-21 19:41:22 +0200

    feat: Prix initial barré

--- src/components/ArticleLabel.tsx
+++ src/components/ArticleLabel.tsx
@@ -2,10 +2,12 @@ import type { ArticleLabelData } from "../dataStructure"
 
 export default function ArticleLabel({ prix, remise, discounted }: ArticleLabelData) {
     return (
-        <div className="flex flex-col text-center gap-4 w-full ">
-            <div className="h-6 w-full">{prix || ' '}</div>
-            <div className="h-6 w-full">{remise || ' '}</div>
-            <div className="h-6 w-full">{discounted || ' '}</div>
+        <div className="flex flex-col text-center gap-4 w-full">
+            <div className="h-6 w-full relative flex items-center justify-center">
+                <span className="line-through decoration-black decoration-2">{prix || ' '}</span>
+            </div>
+            <span className="h-6 w-full">{remise || ' '}</span>
+            <span className="h-6 w-full">{discounted || ' '}</span>
         </div>
     )
 }
