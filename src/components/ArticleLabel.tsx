5221b36e451a142a288e0ec57af3c7d2c5ca01d4
EmilPhOniX <emilien.derobert@gmail.com>
2026-04-21 21:33:51 +0200

    feat: Ajout du sigle %

--- src/components/ArticleLabel.tsx
+++ src/components/ArticleLabel.tsx
@@ -6,7 +6,7 @@ export default function ArticleLabel({ prix, remise, discounted }: ArticleLabelD
             <div className="h-6 w-full relative flex items-center justify-center">
                 <span className="line-through decoration-black decoration-2">{prix || ' '}</span>
             </div>
-            <span className="h-6 w-full">{remise || ' '}</span>
+            <span className="h-6 w-full">{remise ? remise + "%" : ' '}</span>
             <span className="h-6 w-full">{discounted || ' '}</span>
         </div>
     )
